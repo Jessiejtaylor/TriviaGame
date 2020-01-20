b155a2a23a4bf0aed26781061dc018f3eef5bbce
Jessie Taylor <jtaylor@jumpcrew.com>
2020-01-20 14:45:30 -0600

    final commit after css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,87 +9,181 @@
 // 9. Keep track of the right answers, wrong answers, and unanswered according to the users click
 // 10. Show the score with right answers, wrong answers, and unanswered
 
-$("#startButton").show()
-$("#scoreBoard").hide()
-$("#questions").hide()
+$("#startButton").show();
+$("#scoreBoard").hide();
+$("#questions").hide();
 
 // Object is a container that groups all variables together. We need to group bc the variables have a relationship. We have to have right answers, possible, and unanswered - all related to one task.
 // if you have two []'s, you'll need two for loops, one to display the first one and another for the second
-var arrayDataBase = [{
+var arrayDataBase = [
+  {
     question: "Who was Miley Cyrus married to?",
 
     choices: ["Zac Efron", "Liam Hemsworth", "Caitlin Carter", "Cody Simpson"],
 
     //  no array bc theres only one value
     rightAnswer: "Liam Hemsworth"
-}, {
+  },
+  {
     question: "What is Kylie Jenner's daugher's name?",
 
     choices: ["True", "Stormi", "Dream", "Saint"],
 
     rightAnswer: "Stormi"
-}]
+  },
+  {
+    question: "Who is Justin Beiber's new song 'Yummy' about?",
+
+    choices: ["Sofia Richie", "Kylie Jenner", "Selena Gomez", "Hailey Baldwin"],
+
+    rightAnswer: "Hailey Baldwin"
+  },
+  {
+    question: "Who is Sofia Richie dating?",
+
+    choices: ["Drake", "Younes Benjima", "Scott Disick", "Travis Scott"],
+
+    rightAnswer: "Scott Disick"
+  }
+];
 // array is [], object is {}. Array access's content starting from index 0, for the obj, you access content through . operator. ex. "objectname.propertyname"
 
-var rightAnswer = 0
-var wrongAnswer = 0
-var unanswered = 0
+var rightAnswer = 0;
+var wrongAnswer = 0;
+var unanswered = 0;
 
-var remainingTime = 28
+var remainingTime = 30;
+var timerInterval;
 
+// start the trivia
 function initializeTrivia() {
-    rightAnswer = 0
-    wrongAnswer = 0
-    unanswered = 0
+  rightAnswer = 0;
+  wrongAnswer = 0;
+  unanswered = 0;
 
-    remainingTime = 28
+  remainingTime = 30;
 
-    $("#startButton").show()
-    $("#scoreBoard").hide()
-    $("#questions").hide()
-
-
+  $("#startButton").show();
+  //   $("#scoreBoard").hide();
+  $("#questions").hide();
 }
 
-initializeTrivia()
+initializeTrivia();
 
-$("#startButton").on("click", function () {
-    $("#startButton").hide()
-    $("#questions").show()
-    $("#scoreBoard").hide()
+$("#startButton").on("click", function() {
+  $("#startButton").hide();
+  $("#questions").show();
+  $("#scoreBoard").hide();
+  setup();
+  timerInterval = setInterval(countDown, 1000);
+});
 
-    setInterval(countDown, 1000)
-})
+function setup() {
+  $("#questions").empty();
+  var div = $("<div>");
+  //   try inline to get the timer and "remaining time" on the same line
+  div.text("Remaining Time: ");
+  div.append('<div id="time_count"></div>');
+  $("#questions").append(div);
+
+  for (let index = 0; index < arrayDataBase.length; index++) {
+    // creating a dynamic H2
+    var h2 = $("<h2>");
+    h2.html(arrayDataBase[index].question);
+    $("#questions").append(h2);
+
+    // second for loop is to display the choices
+    for (
+      let indexChoices = 0;
+      indexChoices < arrayDataBase[index].choices.length;
+      indexChoices++
+    ) {
+      var choice = arrayDataBase[index].choices[indexChoices];
+      var input = $("<input type='radio'>");
+      input.attr("name", arrayDataBase[index].question);
+      console.log(choice);
+      // attr is anything before the = sign
+      input.attr("value", choice);
+
+      //   this is the section im working on 1/17/20
+      input.attr("data-answer", arrayDataBase[index].rightAnswer);
+      input.attr("class", "radioBtn");
+      // input is button, choice is text
+      $("#questions").append(input, choice); // removed , choice
+    }
+  }
+}
+$(".radioBtn").on("click", function() {
+  var getCurrentClicked = $(this).val();
+  var getStoredDataAnswer = $(this).attr("data-answer");
+  console.log(getCurrentClicked);
+  if (getCurrentClicked === getStoredDataAnswer) {
+    // as a way to test for correct
+    alert("correct");
+  } else {
+    alert("wrong");
+  }
+});
+
+function onTimerEnd() {
+  // if selected answer == right => Correct
+
+  // else => Incorrect
+
+  //console.log($("input"));
+  var i_list = $("input");
+  for (var i = 0; i < i_list.length; i++) {
+    // console.log(i_list[i].checked);
+    if (i_list[i].checked) {
+      // change to be checking the value in the database, search by the name that i have bc all of the names have a reference to the question
+      //   console.log(i_list[i].value);
+      //   console.log(i_list[i].name);
+      // loop thru all questions, match on each iteration
+      // when found, compare rightAnswer to i_list[i].value
+      for (var j = 0; j < arrayDataBase.length; j++) {
+        if (arrayDataBase[j].question === i_list[i].name) {
+          //   console.log(
+          //     i_list[i].value +
+          //       " " +
+          //       (i_list[i].value === arrayDataBase[j].rightAnswer)
+          //   );
+          if (i_list[i].value === arrayDataBase[j].rightAnswer) {
+            // correctness
+            rightAnswer++;
+          } else {
+            wrongAnswer++;
+          }
+        }
+      }
+    }
+  }
+  unanswered = 4 - rightAnswer - wrongAnswer;
+  updateScores();
+  clearInterval(timerInterval);
+
+  initializeTrivia();
+}
+
+function updateScores() {
+  $("#scoreBoard").text(
+    `Right: ${rightAnswer}
+    Wrong: ${wrongAnswer}
+    Unanswered: ${unanswered}`
+  );
+  $("#scoreBoard").show();
+}
 
 function countDown() {
-    remainingTime--
-    // .empty resets the whole div to blank
-    $("#questions").empty()
-    var div = $("<div>")
-    div.html("Remaining Time: " + remainingTime)
-    $("#questions").append(div)
+  // update time dependent elements
+  remainingTime--;
+  // .empty resets the whole div to blank
+  //   $("#questions").empty();
+  //
+  $("#time_count").text(remainingTime);
+  if (remainingTime <= 0) {
+    onTimerEnd();
+  }
+  //
 
-    // index = itterator, < = ending condition, index++ = incriment (by 1 in this case)
-    for (let index = 0; index < arrayDataBase.length; index++) {
-        // creating a dynamic H2
-        var h2 = $("<h2>")
-        h2.html(arrayDataBase[index].question)
-        $("#questions").append(h2)
-
-        // second for loop is to display the choices
-        for (let indexChoices = 0; indexChoices < arrayDataBase[index].choices.length; indexChoices++) {
-
-            var choice = arrayDataBase[index].choices[indexChoices]
-            var input = $("<input type='radio'>")
-            console.log(choice)
-            // attr is anything before the = sign
-            input.attr("value", choice)
-
-            // input is button, choice is text
-            $("#questions").append(input, choice)
-        }
-
-    }
-
-
+  // index = itterator, < = ending condition, index++ = incriment (by 1 in this case)
 }
